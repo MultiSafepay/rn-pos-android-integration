6cbcac854cbf1bcd43881fa7000c3123de15f0cf
Javier Algaba Borrego <javier.algaba@multisafepay.com>
2026-07-23 11:00:14 +0200

    Adapt to the latest Tap to Pay changes

--- android/src/main/java/expo/modules/rnposandroidintegration/TransactionStatus.kt
+++ android/src/main/java/expo/modules/rnposandroidintegration/TransactionStatus.kt
@@ -4,5 +4,6 @@ enum class TransactionStatus(val value: String) {
     EXCEPTION("EXCEPTION"),
     COMPLETED("COMPLETED"),
     CANCELLED("CANCELLED"),
-    DECLINED("DECLINED")
+    DECLINED("DECLINED"),
+    UNDEFINED("UNDEFINED")
 }
